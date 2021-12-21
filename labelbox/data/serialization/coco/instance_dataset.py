c835ab8138ec2259e769b16b9ae1128b17cc887d
f-silverio <fjlsilverio@gmail.com>
2021-12-21 16:18:31 +0000

    refactor: WIP

--- labelbox/data/serialization/coco/instance_dataset.py
+++ labelbox/data/serialization/coco/instance_dataset.py
@@ -4,6 +4,7 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 from typing import Any, Dict, List, Tuple
 from pathlib import Path
 
+from loguru import logger
 import numpy as np
 from tqdm import tqdm
 from pydantic import BaseModel
@@ -101,22 +102,28 @@ def process_label(
     coco_annotations = []
     annotation_lookup = get_annotation_lookup(label.annotations)
     categories = {}
+    annotation_names = []
+    hash_cat = None
     for class_name in annotation_lookup:
         for annotation in annotation_lookup[class_name]:
-            if annotation.name not in categories:
-                categories[annotation.name] = hash_category_name(
-                    annotation.name)
+            annotation_names.append(annotation.name)
+            if annotation.name not in categories.keys():
+                hash_cat = hash_category_name(annotation.name)
+                categories[hash_cat] = annotation.name
+                # print("Adding {} as {}".format(annotation.name, categories[annotation.name]))
             if isinstance(annotation.value, Mask):
                 coco_annotations.append(
                     mask_to_coco_object_annotation(annotation, annot_idx,
                                                    image_id,
-                                                   categories[annotation.name]))
+                                                   hash_cat))
             elif isinstance(annotation.value, (Polygon, Rectangle)):
                 coco_annotations.append(
                     vector_to_coco_object_annotation(
                         annotation, annot_idx, image_id,
-                        categories[annotation.name]))
+                        hash_cat))
             annot_idx += 1
+    # print(set(annotation_names))
+    # print(categories)
     return image, coco_annotations, categories
 
 
@@ -153,24 +160,32 @@ class CocoInstanceDataset(BaseModel):
                 process_label(label, idx, image_root, cloud_provider, azure_storage_container)
                 for idx, label in enumerate(labels)
             ]
-
+        logger.info([i[2] for i in results])
         for result in results:
             images.append(result[0])
             all_coco_annotations.extend(result[1])
             coco_categories.update(result[2])
-
+        logger.info("####### Coco Cats")
+        logger.info(coco_categories)
         category_mapping = {
             category_id: idx + 1
-            for idx, category_id in enumerate(coco_categories.values())
+            for idx, category_id in enumerate(set(coco_categories.values()))
         }
+        logger.info("####### Category Mapping")
+        logger.info(category_mapping)
         categories = [
-            Categories(id=category_mapping[idx],
+            Categories(id=idx,
                        name=name,
                        supercategory='all',
-                       isthing=1) for name, idx in coco_categories.items()
+                       isthing=1) for name, idx in category_mapping.items()
         ]
+        logger.info("####### All coco annotations")
+        # print([ann.category_id for ann in all_coco_annotations])
         for annot in all_coco_annotations:
-            annot.category_id = category_mapping[annot.category_id]
+            # print(category_mapping)
+            logger.info(annot.category_id)
+            coco_cat_name = coco_categories[annot.category_id]
+            annot.category_id = category_mapping[coco_cat_name]
 
         return CocoInstanceDataset(info={'image_root': image_root},
                                    images=images,
